b77b93136ffdc03bcf9a2c4e73112dbf14794e1b
Yoshitaka Hirata <hirata@hogehoge.com>
2024-05-31 12:21:36 +0900

    add

--- DP.c
+++ DP.c
@@ -31,7 +31,7 @@ int main(void) {
     double g[MAXSIZE][MAXSIZE];
     double T[ALLFILE];
     double T_min;
-    int match_Idx = 0;
+    int T_min_Idx = 0;
     int count = 0;
 
     printf("テンプレートの音声データを以下から番号で選択してください。\n 1.city011 2.city012 3.city021 4.city022 : "); scanf("%d",&tmp);
@@ -40,7 +40,7 @@ int main(void) {
     printf("未知の音声データを以下から番号で選択してください。\n 1.city011 2.city012 3.city021 4.city022 : "); scanf("%d",&unknown);
     unknown=getCityNum(unknown);
 
-    printf("\nテンプレートの音声データ : city%03d\n未知の音声データ : city%03d\nこの2つのデータを音声認識をさせます。\n\n", tmp, unknown);
+    printf("\nテンプレートの音声データ : city%03d\n未知の音声データ : city%03d\nこの2つのデータの音声認識をさせます。\n\n", tmp, unknown);
 
     for (int tmp_Idx = 0; tmp_Idx < ALLFILE; tmp_Idx++) {
         sprintf(ch, "/home/hirata/recognition/city_mcepdata/city%.3d/city%.3d_%.3d.txt", tmp, tmp, tmp_Idx + 1);
@@ -89,7 +89,7 @@ int main(void) {
             }
             fclose(fp_unknown);
 
-            //デフォルトの距離を生成　ユークリッド距離
+            //局所距離を生成　ユークリッド距離
             for (int i = 0; i < city_tmp.flame; i++) {
                 for (int j = 0; j < city_unknown.flame; j++) {
                     d[i][j] = 0;
@@ -111,7 +111,7 @@ int main(void) {
             for (int i = 1; i < city_tmp.flame; i++) {
                 for (int j = 1; j < city_unknown.flame; j++) {
                     double a = g[i][j - 1] + d[i][j];
-                    double b = g[i - 1][j - 1] + 2 * d[i][j];
+                    double b = g[i - 1][j - 1] + 2* d[i][j];
                     double c = g[i - 1][j] + d[i][j];
                     g[i][j] = fmin(a, fmin(b, c));
                 }
@@ -119,15 +119,28 @@ int main(void) {
 
             T[unknown_Idx] = g[city_tmp.flame - 1][city_unknown.flame - 1] / (city_tmp.flame + city_unknown.flame);//単語間距離
             T_min = (unknown_Idx == 0) ? T[unknown_Idx] : fmin(T_min, T[unknown_Idx]);//最小の単語間距離
-            match_Idx = (T_min == T[unknown_Idx]) ? unknown_Idx : match_Idx;//最小の単語間距離の未知の単語のファイル番号(インデックス)
+            T_min_Idx = (T_min == T[unknown_Idx]) ? unknown_Idx : T_min_Idx;//最小の単語間距離の未知の単語のファイル番号(インデックス)
         }
 
-        if (match_Idx == tmp_Idx) {
+        if (T_min_Idx == tmp_Idx) {
             count++;//一致していれば次に進む
         } else {
             printf("以下のテンプレートの音声データと未知の音声データが一致しているという誤認識をしました。\n");
             printf("テンプレートの音声データ : city%03d/city%03d_%03d.txt\n", tmp, tmp, tmp_Idx + 1);
-            printf("未知の音声データ : city%03d/city%03d_%03d.txt\n\n", unknown, unknown, match_Idx + 1);
+            printf("発声内容 : %s",city_tmp.word);
+            printf("未知の音声データ : city%03d/city%03d_%03d.txt\n", unknown, unknown, T_min_Idx + 1);
+            sprintf(ch, "/home/hirata/recognition/city_mcepdata/city%.3d/city%.3d_%.3d.txt", unknown, unknown, T_min_Idx + 1);
+            //ファイルを読み込みモードで開く
+            fp_unknown = fopen(ch, "r");
+            //ファイル読み取りエラー処理
+            if (fp_unknown == NULL) {
+                printf("未知の音声データが開けませんでした。\n");
+                exit(EXIT_FAILURE);
+            }
+            fgets(city_unknown.name, sizeof(city_unknown.name), fp_unknown);//ファイル名取得
+            fgets(city_unknown.word, sizeof(city_unknown.word), fp_unknown);//単語取得
+            fclose(fp_unknown);
+            printf("発声内容 : %s\n",city_unknown.word);
         }
     }
     printf("認識率 %d%% です。\n", count);
